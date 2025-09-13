f6f53335f940baf84154c4f98e4484a42f3c0028
Camerin <figueroa0609@protonmail.com>
2025-09-12 21:38:36 -0400

    Refactored to have main menu which it reverts to after the game is over

--- game/game.gd
+++ game/game.gd
@@ -2,12 +2,13 @@ extends Node2D
 
 @onready var buttons: Array[Button] = [$"1", $"2", $"3", $"4", $"5", $"6", $"7", $"8", $"9"];
 @onready var label: RichTextLabel = $Label;
+@onready var game_over_timer: Timer = $GameOverTimer;
 
 var has_won: String = "";
 var turn: String = "X";
 
 func _ready() -> void:
-  setup_board()
+  setup_board();
 
 # Used to initially setup the game. This can be used to reset the board also.
 func setup_board() -> void:
@@ -17,63 +18,67 @@ func setup_board() -> void:
   label.text = "";
   turn = "X";
   has_won = "";
-  set_turn_label()
+  set_turn_label();
 
 # Takes an id for the button that was pressed and attempts to use that as a turn
 func _on_pressed(id: int) -> void:
   
   # If noone has won and the button doesn't already have a mark
   if buttons[id-1].text == "" && has_won == "":
-    
     # Set the button to the current turn
     buttons[id-1].text = turn;
-    
-    # flip the turn
-    turn = "X" if turn == "O" else "O"
-    
-    set_turn_label()
+  
+  # flip the turn
+  turn = "X" if turn == "O" else "O";
+  
+  set_turn_label();
 
-    check_win()
+  check_win();
 
 func set_turn_label():
   # Set the label
-  label.text = "Turn: " + turn
+  label.text = "Turn: " + turn;
 
 # Checks if there is a win. If there is has_won will be set and the label text will be changed
 func check_win() -> void:
   # Row win
   for i in range(3):
     if (
-        buttons[(i*3)+2].text != ""
-        && buttons[(i*3)+0].text == buttons[(i*3)+1].text
-        && buttons[(i*3)+1].text == buttons[(i*3)+2].text
+      buttons[(i*3)+2].text != ""
+      && buttons[(i*3)+0].text == buttons[(i*3)+1].text
+      && buttons[(i*3)+1].text == buttons[(i*3)+2].text
       ):
-      has_won = buttons[(i*3)+2].text
+      has_won = buttons[(i*3)+2].text;
 
   # Column Win
   for i in range(3):
     if (
-        buttons[i].text != ""
-        && buttons[i].text == buttons[i+3].text
-        && buttons[i+3].text == buttons[i+6].text
+      buttons[i].text != ""
+      && buttons[i].text == buttons[i+3].text
+      && buttons[i+3].text == buttons[i+6].text
       ):
-      has_won = buttons[i].text
+      has_won = buttons[i].text;
 
   # Win via cross
   if (
-      buttons[4].text != ""
-      && (
-        (
-          buttons[4].text == buttons[0].text
-          && buttons[4].text == buttons[8].text
-        )
-        || (
-          buttons[4].text == buttons[2].text
-          && buttons[4].text == buttons[6].text
-        )
-      )
-    ):
-    has_won = buttons[4].text
+    buttons[4].text != ""
+    && (
+    (
+      buttons[4].text == buttons[0].text
+      && buttons[4].text == buttons[8].text
+    )
+    || (
+      buttons[4].text == buttons[2].text
+      && buttons[4].text == buttons[6].text
+    )
+    )
+  ):
+    has_won = buttons[4].text;
 
   if has_won != "":
-    label.text = has_won + " Has won"
+   label.text = has_won + " Has won";
+   game_over_timer.start();
+
+
+func _on_game_over_timer_timeout() -> void:
+  Globals.game_over.emit();
---
+++ menus/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var main_scene = load("res://game/game.tscn");
+@onready var start_button = $StartButton;
+
+var current_scene = null;
+
+func _ready():
+  Globals.game_over.connect(_game_over);
+
+func _game_over():
+  print("Game Over");
+  if current_scene != null:
+    get_node("/root").remove_child(current_scene)
+    current_scene.call_deferred("free")
+
+  start_button.show();
+
+func _on_start_button_pressed() -> void:
+  var scene = main_scene.instantiate();
+
+  get_node("/root").add_child(scene);
+  
+  start_button.hide();
+  
+  current_scene = scene;
